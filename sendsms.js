e3a0270cdd9ad2cc83170bc5861ac6a1f9644272
Kevin Zeng <zengfenfei@gmail.com>
2016-06-14 17:12:46 +0800

    send sms sequentially, handle limit rate exceeding

--- sendsms.js
+++ sendsms.js
@@ -4,7 +4,7 @@ try {
 	console.error('Please run `npm install` first.');
 	return;
 }
-
+var HttpStatus = require('http-status-codes')
 var authConf = require('./conf/auth.json');
 var smsConf = require('./conf/sms.json');
 
@@ -12,12 +12,12 @@ var ringcentral = new Ringcentral(authConf.app);
 
 var rcPlatform = ringcentral.platform();
 
-rcPlatform.login(authConf.user).then(sendSms).catch(function(e) {
+rcPlatform.login(authConf.user).then(composeMessages).then(sendSms).catch(function(e) {
 	console.error('Fail to login:' + e);
 });
 
-function sendSms() {
-	var sentCount = 0;
+function composeMessages() {
+	var messages = [];
 	var fromPhones = smsConf.from;
 	var repeat = smsConf.count || 1;
 	for (var n = 1; n <= repeat; n++) {
@@ -29,14 +29,32 @@ function sendSms() {
 				to: smsConf.to,
 				text: smsConf.text.replace('{signature}', signature)
 			};
-			rcPlatform.post('/account/~/extension/~/sms', params).then(function() {
-				sentCount++;
-				console.log(sentCount + ' sms sent.');
-			}).catch(function(e) {
-				console.error('Fail to send sms:' + e);
-			});
+			messages.push(params);
 		}
 	}
+	return messages;
+}
+
+var sentCount = 0;
+
+function sendSms(remainingMessages) {
+	rcPlatform.post('/account/~/extension/~/sms', remainingMessages[0]).then(function() {
+		remainingMessages.shift();
+		sentCount++;
+		console.log(sentCount + ' sms sent.');
+		if (remainingMessages.length > 0) {
+			sendSms(remainingMessages);
+		}
+	}).catch(function(e) {
+		var res = e.apiResponse.response();
+		if (res.status = HttpStatus.TOO_MANY_REQUESTS) {
+			var retryAfter = parseInt(res.headers.get('retry-after'));
+			console.warn('Rate limit reached, will retry after ' + retryAfter + ' seconds.');
+			setTimeout(sendSms, retryAfter * 1000, remainingMessages);
+		} else {
+			console.error('Fail to send sms:' + e);
+		}
+	});
 }
 
 function phoneNumberFmt(info) {
