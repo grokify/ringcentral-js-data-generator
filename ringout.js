0128657e4fc5ca9c5108fe76529104fc22b3b866
Kevin Zeng <zengfenfei@gmail.com>
2016-06-17 16:09:49 +0800

    generate phone call logs repeatedly

--- ringout.js
+++ ringout.js
@@ -14,17 +14,34 @@ var ringcentral = new Ringcentral(authConf.app);
 
 var rcPlatform = ringcentral.platform();
 
-rcPlatform.login(authConf.user).then(startRingout).catch(function(e) {
+rcPlatform.login(authConf.user).then(makeRingoutsRepeatedly).catch(function(e) {
 	console.error('Fail to login:' + e);
 });
 
 
-var sentCount = 0;
 var repeated = 0;
-function startRingout() {
-	ringout(ringConf.params, rcPlatform).then(function(theRingout) {
-		console.log('ringout result', theRingout);
-	}).catch(function (err) {
-		console.error('ringout error', err);
+
+function makeRingoutsRepeatedly() {
+	repeated++;
+	var repeat = ringConf.count || 1;
+	if (repeated > repeat && repeat != -1) {
+		return;
+	}
+	Promise.all(makeRingouts(ringConf.ringouts)).then(makeRingoutsRepeatedly).catch(function() {
+		setTimeout(makeRingoutsRepeatedly, 3000);
 	});
+
+}
+
+function makeRingouts(ringouts) {
+	var results = [];
+	for (var i = 0; i < ringouts.length; i++) {
+		results.push(ringout(ringouts[i], rcPlatform).then(function(theRingout) {
+			console.log('ringout result', theRingout);
+			return theRingout;
+		}).catch(function(e) {
+			console.error('ringout error ' + e);
+		}));
+	}
+	return results;
 }
